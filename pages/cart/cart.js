70dec61257f1f2ad5d58d5372d698ac8ce4ea6e7
Elkin Silva <elkinsilvamanrique@gmail.com>
2023-04-10 23:18:21 -0500

    fix all app

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,11 +1,61 @@
 Page({
   data: {
-    cartItems: []
+    cartItems: [],
+    piezas: {
+      x: 0,
+      y: 0
+    },
+    rompecabezasCompleto: false,
+    mostrarRompecabezas: false
   },
+
   onLoad(options) {
     const cartItems = JSON.parse(options.cartItems);
     console.log(cartItems);
     this.setData({ cartItems });
   },
 
+  onTouchMove(e) {
+    console.log(e);
+    const { x, y } = e.detail;
+    console.log(e.detail);
+    const { piezas } = this.data;
+    console.log(piezas);
+    console.log(x, y);
+    const id = e.target.id;
+    if (id === "pieza2") {
+      console.log(`Soy ${id}`);
+      if (piezas.x >= 90 && piezas.y <= 0) {
+        this.setData({ rompecabezasCompleto: true });
+      }
+      console.log(`${x} y ${y}`);
+    }
+    this.setData({
+      "piezas.x": x,
+      "piezas.y": y
+    });
+  },
+
+  onConfirmarCompra() {
+    this.setData({
+      mostrarRompecabezas: true
+    });
+    if (this.data.rompecabezasCompleto) {
+      my.alert({
+        title: "¡Compra confirmada!",
+        content: "¡Gracias por tu compra!",
+        showCancel: false,
+        buttonText: "Aceptar",
+        success: () => {
+          my.navigateBack({ delta: 1 });
+        }
+      });
+    } else {
+      my.alert({
+        title: "Se necesita completar el rompecabezas para confirmar la compra",
+        buttonText: "Aceptar",
+        duration: 2000
+      });
+    }
+  }
 });
--- pages/screen/screen.js
+++ pages/screen/screen.js
@@ -1,49 +1,33 @@
 Page({
   data: {
-    productList: [
-      {
-        id: 1,
-        name: "Producto 1",
-        description: "Descripción del producto 1",
-        price: 10.99
-      },
-      {
-        id: 2,
-        name: "Producto 2",
-        description: "Descripción del producto 2",
-        price: 15.99
-      },
-      {
-        id: 3,
-        name: "Producto 3",
-        description: "Descripción del producto 3",
-        price: 20.99
-      },
-      {
-        id: 4,
-        name: "Producto 4",
-        description: "Descripción del producto 4",
-        price: 25.99
-      },
-      {
-        id: 5,
-        name: "Producto 5",
-        description: "Descripción del producto 5",
-        price: 30.99
-      }
-    ],
+    productList: [],
     cartItems: []
   },
 
+  onLoad(options) {
+    // Generar 30 productos
+    const productList = [];
+    for (let i = 1; i <= 30; i++) {
+      productList.push({
+        id: i,
+        name: `Producto ${i}`,
+        description: `Descripción del producto ${i}`,
+        price: Math.floor(Math.random() * 100) + 0.99
+      });
+    }
+
+    this.setData({ productList });
+  },
   addToCart(event) {
     const itemId = event.currentTarget.dataset.itemId;
     const item = this.data.productList.find(item => item.id === itemId);
     if (!item) return;
     this.setData({ cartItems: [...this.data.cartItems, item] });
     console.log(item);
-    my.showToast({
+    my.alert({
       content: "Item añadido al carrito de compras",
-      delay: 100
+      showCancel: false,
+      buttonText: "Aceptar"
     });
   },
 
